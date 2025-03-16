e520fc6949a4aa17006c9f2ec52cef755b4a6d53
Sebi <sebastianstewart03@gmail.com>
2025-03-16 17:16:16 +0000

    Adding the location to the main program

--- DistanceTracker/ContentView.swift
+++ DistanceTracker/ContentView.swift
@@ -6,16 +6,73 @@
 //
 
 import SwiftUI
+import Combine
+import Darwin
+import ContactsUI
 
 struct ContentView: View {
+    @StateObject var deviceLocationService = DeviceLocationService.shared
+    
+    @State var tokens: Set<AnyCancellable> = []
+    @State var coordinates: (lat: Double, lon: Double) = (0, 0)
+    @State var distance: Double = 0
+    var target: (lat: Double, lon: Double) = (54.9783, -1.6178)
+    
     var body: some View {
+        locationStack
+    }
+    
+    private var locationStack: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Latitutde: \(coordinates.lat)")
+                .font(.title)
+            Text("Longitude: \(coordinates.lon)")
+                .font(.title)
+            Text("Distance: \((distance * 10).rounded() / 10) km")
         }
         .padding()
+        .onAppear {
+            observeCoordinateUpates()
+            observeLocationAccessDenied()
+            deviceLocationService.requestLocationUpdates()
+        }
+    }
+    
+    func observeCoordinateUpates() {
+        deviceLocationService.coordinatesPublisher
+            .receive(on: DispatchQueue.main)
+            .sink {completion in
+                if case .failure(let error) = completion {
+                    print(error)
+                }
+            } receiveValue: { coordinates in
+                self.coordinates = (coordinates.latitude, coordinates.longitude)
+                self.distance = updateDistanceToTarget()
+            }
+            .store(in : &tokens)
+    }
+    
+    func observeLocationAccessDenied() {
+        deviceLocationService.deniedLocationAccessPublisher
+            .receive(on: DispatchQueue.main)
+            .sink {
+                print("Show some kind of alert to the user")
+            }
+            .store(in: &tokens)
+    }
+    
+    func updateDistanceToTarget() -> Double{
+        let radius: Double = 6371.0 //km
+        let pi = Double.pi
+        
+        let a = (0.5 - cos((target.lat - coordinates.lat) * pi) / 2
+                     + cos(coordinates.lat * pi) * cos(target.lat * pi) *
+                 (1 - cos((target.lon - coordinates.lon) * pi)) / 2)
+        return (2 * radius * asin(sqrt(a)))
+        
     }
 }
 
---
+++ DistanceTracker/DeviceLocationService.swift
@@ -0,0 +1,65 @@
+//
+//  DeviceLocationService.swift
+//  DistanceTracker
+//
+//  Created by Sebastian Stewart on 16/03/2025.
+//
+
+
+import Combine
+import CoreLocation
+
+class DeviceLocationService: NSObject, CLLocationManagerDelegate, ObservableObject {
+    
+    var coordinatesPublisher = PassthroughSubject<CLLocationCoordinate2D, Error>()
+    
+    var deniedLocationAccessPublisher = PassthroughSubject<Void, Never>()
+    
+    private override init(){
+        super.init()
+    }
+    
+    static let shared = DeviceLocationService()
+    
+    private lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        return manager
+    }()
+    
+    func requestLocationUpdates() {
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.startUpdatingLocation()
+            
+        default:
+            deniedLocationAccessPublisher.send()
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        
+        case .authorizedWhenInUse, .authorizedAlways:
+            manager.startUpdatingLocation()
+            
+        default:
+            manager.stopUpdatingLocation()
+            deniedLocationAccessPublisher.send()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        coordinatesPublisher.send(location.coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        coordinatesPublisher.send(completion: .failure(error))
+    }
+}
