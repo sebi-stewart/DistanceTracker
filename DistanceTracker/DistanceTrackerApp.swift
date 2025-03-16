8de8eb17d24fe484b5973dd6404f15f9f7d63db0
Sebi <sebastianstewart03@gmail.com>
2025-03-16 13:56:20 +0000

    Initial Commit

---
+++ DistanceTracker/DistanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistanceTrackerApp.swift
+//  DistanceTracker
+//
+//  Created by Sebastian Stewart on 16/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DistanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
